dd6b7f4ea3a54e5cbd718d90690280cfe93723bb
Siddharth Ancha <sancha@mit.edu>
2024-11-11 13:08:39 -0800

    Move env code to dp_state_notebook.py

--- flow_policy/pusht/dataset.py
+++ flow_policy/pusht/dataset.py
@@ -3,20 +3,29 @@ import torch
 import zarr
 
 from .dp_state_notebook import (
-    create_sample_indices, sample_sequence, get_data_stats, normalize_data,
+    create_sample_indices, get_data_stats, normalize_data, PushTStateDataset,
 )
 
-class PushTStateDatasetActionUpdt (torch.utils.data.Dataset):
+class PushTStateObsDatasetWithObsAsActions (PushTStateDataset):
     """
-    Author: Sunshine Jiang
+    A dataset of where gripper observations act as actions.
     """
-    def __init__(self, dataset_path,
-                 pred_horizon, obs_horizon, action_horizon):
+    def __init__(
+            self,
+            dataset_path: str,
+            pred_horizon: int,
+            obs_horizon: int,
+            action_horizon: int,
+        ):
 
         # read from zarr dataset
         dataset_root = zarr.open(dataset_path, 'r')
-         # Marks one-past the last index for each episode
+
+        # Marks one-past the last index for each episode
         episode_ends = dataset_root['meta']['episode_ends'][:]
+
+        train_data = self.MakeTrainData(dataset_root, episode_ends)
+
         # compute start and end of each state-action sequence
         # also handles padding
         indices = create_sample_indices(
@@ -25,28 +34,6 @@ class PushTStateDatasetActionUpdt (torch.utils.data.Dataset):
             # add padding such that each timestep in the dataset are seen
             pad_before=obs_horizon-1,
             pad_after=action_horizon-1)
-        
-        # All demonstration episodes are concatinated in the first dimension N
-        states = dataset_root['data']['state'][:]
-        # Initialize a list to store the sliced and shifted episodes
-        shifted_episodes = []
-        # Iterate through the episode ends to slice and shift the tensor
-        start_idx = 0
-        for end_idx in episode_ends:
-            episode = states[start_idx:end_idx]
-            shifted_episode = np.concatenate((episode[1:], episode[-1].reshape(1, -1)), axis=0)
-            shifted_episodes.append(shifted_episode)
-            start_idx = end_idx
-
-        # Combine the shifted episodes back into a single tensor if needed
-        shifted_states = np.concatenate(shifted_episodes, axis=0)
-        train_data = {
-            # (N, action_dim)
-            'action': shifted_states[:, :2],
-            # (N, obs_dim)
-            'obs': dataset_root['data']['state'][:]
-        }
-       
 
         # compute statistics and normalized data to [-1,1]
         stats = dict()
@@ -62,25 +49,28 @@ class PushTStateDatasetActionUpdt (torch.utils.data.Dataset):
         self.action_horizon = action_horizon
         self.obs_horizon = obs_horizon
 
-    def __len__(self):
-        # all possible segments of the dataset
-        return len(self.indices)
+    @staticmethod
+    def MakeTrainData(dataset_root, episode_ends):
+        # All demonstration episodes are concatinated in the first dimension N
+        states = dataset_root['data']['state'][:]
+        # Initialize a list to store the sliced and shifted episodes
+        shifted_episodes = []
+        # Iterate through the episode ends to slice and shift the tensor
+        start_idx = 0
+        for end_idx in episode_ends:
+            episode = states[start_idx:end_idx]
+            shifted_episode = np.concatenate((episode[1:], episode[-1].reshape(1, -1)), axis=0)
+            shifted_episodes.append(shifted_episode)
+            start_idx = end_idx
+        
 
-    def __getitem__(self, idx):
-        # get the start/end indices for this datapoint
-        buffer_start_idx, buffer_end_idx, \
-            sample_start_idx, sample_end_idx = self.indices[idx]
+        # Combine the shifted episodes back into a single tensor if needed
+        shifted_states = np.concatenate(shifted_episodes, axis=0)
+        train_data = {
+            # (N, action_dim)
+            'action': shifted_states[:, :2],
+            # (N, obs_dim)
+            'obs': dataset_root['data']['state'][:]
+        }
 
-        # get nomralized data using these indices
-        nsample = sample_sequence(
-            train_data=self.normalized_train_data,
-            sequence_length=self.pred_horizon,
-            buffer_start_idx=buffer_start_idx,
-            buffer_end_idx=buffer_end_idx,
-            sample_start_idx=sample_start_idx,
-            sample_end_idx=sample_end_idx
-        )
-
-        # discard unused observations
-        nsample['obs'] = nsample['obs'][:self.obs_horizon,:]
-        return nsample
+        return train_data
